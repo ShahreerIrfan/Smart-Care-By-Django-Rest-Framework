ab599dcbe41e160a6ae583903a229f5e5d151829
Your Name <mdshahreerirfan@gmail.com>
2024-02-08 01:37:07 +0600

    Serializer is added

--- patient/serializers.py
+++ patient/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from . import models
 
 class PatientSerializers(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(many =False)
     class Meta:
         model = models.Patient
         fields = '__all__'
+
+
--- patient/views.py
+++ patient/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from django.urls import path,include
+from . import models
+from . import serializers
 # Create your views here.
+
+class PatientViewSet(viewsets.ModelViewSet):
+    queryset = models.Patient.objects.all()
+    serializer_class = serializers.PatientSerializers
