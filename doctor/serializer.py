838f4d2c9aff3bb90ff59a73693a30b9caee27d7
Your Name <mdshahreerirfan@gmail.com>
2024-02-08 17:38:09 +0600

    Serializer is added

---
+++ doctor/serializer.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from . import models
+
+class DoctorSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(many=False)
+    class Meta:
+        model = models.Doctor
+        fields = '__all__'
+class DoctorSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(many=False)
+    class Meta:
+        model = models.Doctor
+        fields = '__all__'
+class DoctorSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(many=False)
+    class Meta:
+        model = models.Doctor
+        fields = '__all__'
+class DoctorSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(many=False)
+    class Meta:
+        model = models.Doctor
+        fields = '__all__'
+class DoctorSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(many=False)
+    class Meta:
+        model = models.Doctor
+        fields = '__all__'
